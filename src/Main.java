25f8725bb825a9adad86b29000a24c59da2164ee
MocanuGeorge010 <mocanu.george010@gmail.com>
2023-04-24 22:18:56 +0300

    Added Counter

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,24 @@ public class Main {
         // mai putin iar daca este mai mic decat "13" afiseaza mai  mult.
 
         // Update1: transforma numarul 13 intr-un numar aleatoriu
+        // Update2:
 
         Scanner sc = new Scanner(System.in);
         Random rnd = new Random();
         int hiddenNumber = rnd.nextInt(100);
+        int counter = 0;
 
         while (true) {
             System.out.println("Introduceti un numar");
             int guessedNumber = sc.nextInt();
+            counter++;
 
             if (guessedNumber < hiddenNumber) {
                 System.out.println("Mai mult");
             } else if (guessedNumber > hiddenNumber) {
                 System.out.println("Mai putin");
             } else {
-                System.out.println("Ai ghicit");
+                System.out.println("You guessed using " + counter + " attempts" );
                 break;
             }
         }
