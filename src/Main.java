d81cd72def5349cf6cbe2344b5ca13a9a460827d
MocanuGeorge010 <mocanu.george010@gmail.com>
2023-04-24 22:26:32 +0300

    Removed Comments

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Scrie un program care cere de la user un numar de ghicit, daca acesta este "13" afiseaza "Ai ghicit ", daca este mai mare de "13" afiseaza
-        // mai putin iar daca este mai mic decat "13" afiseaza mai  mult.
-
-        // Update1: transforma numarul 13 intr-un numar aleatoriu
-        // Update2:
-
         Scanner sc = new Scanner(System.in);
         Random rnd = new Random();
         int hiddenNumber = rnd.nextInt(100);
@@ -25,7 +19,7 @@ public class Main {
             } else if (guessedNumber > hiddenNumber) {
                 System.out.println("Less");
             } else {
-                System.out.println("You guessed using " + counter + " attempts" );
+                System.out.println("You guessed using " + counter + " attempts");
                 break;
             }
         }
