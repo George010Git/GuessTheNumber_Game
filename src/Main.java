aad53fc6cdb86cf89a4d9f5951fd02af81490894
MocanuGeorge010 <mocanu.george010@gmail.com>
2023-04-24 22:37:34 +0300

    Added Lives

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,29 @@ public class Main {
         Random rnd = new Random();
         int hiddenNumber = rnd.nextInt(100);
         int counter = 0;
+        int lives = 6;
 
         while (true) {
             System.out.print("Guess the number: ");
             int guessedNumber = sc.nextInt();
             counter++;
 
+            if (lives == 0) {
+                System.out.println("Sorry, No more lives!");
+                break;
+            }
+
             if (guessedNumber < hiddenNumber) {
                 System.out.println("More");
+                lives--;
             } else if (guessedNumber > hiddenNumber) {
                 System.out.println("Less");
+                lives--;
             } else {
                 System.out.println("You guessed using " + counter + " attempts");
                 break;
             }
+            System.out.println("You have " + lives + " remaining lives");
         }
     }
 }
